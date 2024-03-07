473e3a484943d42c2d6d964ab91121f7cfe16fc8
notworsen <76880350+notworsen@users.noreply.github.com>
2024-03-07 14:53:38 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '367ec63a-127c-453a-8fbe-06a1158e2107';
-let proxyIP ="8.222.208.83";
+let proxyIP ="vip.worsen.eu.org";
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
 
